d58a9579a2e3acb07222cc2da69c12e2617ebca9
brianrtara <brianrtara@gmail.com>
2018-11-22 10:09:28 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Title Page</title>
+
+        <!-- Bootstrap CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.3/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+              
+    <div class="container">
+            
+            <div class="row">
+                
+                <div class="col-lg-3 col-lg-offset-4" style="margin-top: 100px">
+                    
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                           
+                           <form action="" method="POST" role="form">
+                               <legend>REGISTER</legend>
+                           
+                               <div class="form-group">
+                                   <label for="">username</label>
+                                   <input type="text" class="form-control" id="" name="username" placeholder="username">
+                               </div>
+
+                              <div class="form-group">
+                                   <label for="">password</label>
+                                   <input type="password" class="form-control" id="" name="password" placeholder="******">
+                               </div>
+                        
+                               <button type="submit" name="submit" class="btn btn-primary">login</button>
+                           </form>
+                           
+                        </div>
+                    </div>
+                    
+                    
+                </div>
+                
+            </div>
+            
+        </div>
+
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Bootstrap JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+</html>
